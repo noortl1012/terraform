9ec5ddfd65558b37892572b78fe495e466487bbe
noortl1012 <nourtlili746@gmail.com>
2024-04-15 21:15:55 +0100

    eheuh

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,60 +1,20 @@
-terraform {
-  backend "azurerm" {
-    backend_service_arm            = "Azure Resource Manager"  // Azure ARM (Azure Resource Manager)
-    backend_azure_rm_resource_group = "terraform" // Azure Resource Group where the storage account is located
-    backend_azure_rm_storage_account_name = "terraformstock" // Azure Storage Account Name
-    backend_azure_rm_container_name       = "terraform-container" // Azure Storage Container Name
-    backend_azure_rm_key                  = "tf/terraform.tfstate" // Path to the state file within the container
-  }
-}
-resource "azurerm_virtual_machine" "linux_vm" {
-  name = "my-simple-vm"
-  location = "West Europe"
-  resource_group_name = "my-resource-group"
-
-  vm_size = "Standard_B1s"
-
-  storage_os_disk {
-    name = "my-os-disk"
-    caching = "ReadWrite"
-    create_option = "FromImage"
-    managed_disk_type = "StandardSSD_LRS"
-    image = {
-      publisher = "Canonical"
-      offer = "Ubuntu Server"
-      sku = "18.04-LTS"
-      version = "latest"
-    }
-  }
-
-  network_interface_ids = [azurerm_network_interface.main.id]
+# Define Azure provider
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_network_interface" "main" {
-  name = "my-vm-nic"
-  location = azurerm_virtual_machine.linux_vm.location
-  resource_group_name = azurerm_virtual_machine.linux_vm.resource_group_name
-
-  ip_configuration {
-    name = "internal"
-    subnet_id = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-
-  depends_on = [azurerm_subnet.internal]
+# Virtual Network
+resource "azurerm_virtual_network" "my_vnet" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.my_resource_group.location
+  resource_group_name = azurerm_resource_group.my_resource_group.name
 }
 
-resource "azurerm_subnet" "internal" {
-  name = "internal-subnet"
-  resource_group_name = azurerm_virtual_machine.linux_vm.resource_group_name
-  address_prefixes = ["10.0.1.0/24"]
-  virtual_network_name = azurerm_virtual_network.main.name
-
-  depends_on = [azurerm_virtual_network.main]
-}
-
-resource "azurerm_virtual_network" "main" {
-  name = "my-vnet"
-  location = azurerm_virtual_machine.linux_vm.location
-  address_space = ["10.0.0.0/16"]
+# Subnet
+resource "azurerm_subnet" "my_subnet" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.my_resource_group.name
+  virtual_network_name = azurerm_virtual_network.my_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
